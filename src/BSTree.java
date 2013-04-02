f11cd837a67f07b9f856d50e39caf77a7779341c
Lucas Bibiano <lucasbibianodossantos@gmail.com>
2013-04-02 12:27:04 -0300

    Arvores BST e RB faltando apenas remocao, falta comprovar com os casos de teste do professor

---
+++ src/BSTree.java
@@ -0,0 +1,252 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class BSTree<T> 
+{
+    private BNode<T> root;
+   
+    public BSTree()
+    {
+	root = new BNode<T>();
+    }
+    
+    public boolean isValid()
+    {
+	Queue<BNode<T>> queue = new ArrayDeque<BNode<T>>();
+
+	queue.offer(root);
+	
+	while (!queue.isEmpty())
+	{
+	    BNode<T> node = queue.poll();
+	    
+	    if (node.isEmpty())
+	    {
+		continue;
+	    }
+	    
+	    if (node != getRoot())
+	    {
+		//se o no eh esquerdo e a chave dele eh maior do que a do pai
+		if (node.isLeft() && node.getKey() > node.getParent().getKey())
+		    return false;
+		
+		//se o no eh direito e a chave dele eh menor do que a do pai
+		if (node.isRight() && node.getKey() < node.getParent().getKey())
+		    return false;
+		
+		queue.offer(node.getLeft());
+		queue.offer(node.getRight());
+	    }
+	}
+	
+	return true;
+    }
+    
+    public void breadthTraversalPrint()
+    {
+	Queue<BNode<T>> queue = new ArrayDeque<BNode<T>>();
+
+	queue.offer(root);
+	
+	while (!queue.isEmpty())
+	{
+	    BNode<T> node = queue.poll();
+	    
+	    if (node.isEmpty())
+	    {
+		System.out.println("E");
+		continue;
+	    }
+	    
+	    System.out.println(node.getValue());
+	    queue.offer(node.getLeft());
+	    queue.offer(node.getRight());
+	}
+    }
+    
+    public void printInOrder()
+    {
+	inorder(root);
+    }
+    
+    private void inorder(BNode<T> node)
+    {
+	if (node.isEmpty())
+	    return;
+	
+	inorder(node.getLeft());
+	System.out.println(node.getValue());
+	inorder(node.getRight());
+    }
+    
+    public void printPostOrder()
+    {
+	postorder(root);
+    }
+    
+    private void postorder(BNode<T> node)
+    {
+	if (node.isEmpty())
+	    return;
+	
+	postorder(node.getLeft());
+	postorder(node.getRight());
+	System.out.println(node.getValue());
+    }
+    
+    public void printPreOrder()
+    {
+	preorder(root);
+    }
+    
+    private void preorder(BNode<T> node)
+    {
+	if (node.isEmpty())
+	    return;
+	
+	System.out.println(node.getValue());	
+	preorder(node.getLeft());
+	preorder(node.getRight());
+    }
+    
+    public T search(int key)
+    {
+	return searchHelper(root, key).getValue();
+    }
+    
+    private BNode<T> searchHelper(BNode<T> node, int key)
+    {
+	if (node.isEmpty())
+	    return null;
+	
+	if (key > node.getKey())
+	    return searchHelper(node.getRight(), key);
+	else if (key < node.getKey())
+	    return searchHelper(node.getLeft(), key);
+	
+	return node;
+    }
+    
+    public T getMin()
+    {
+	return getMinHelper(root).getValue();
+    }
+    
+    private BNode<T> getMinHelper(BNode<T> node)
+    {	
+	while (!node.isEmpty())
+	{
+	    node = node.getLeft();
+	}
+	
+	return node;
+    }
+    
+    public T getMax()
+    {
+	return getMaxHelper(root).getValue();
+    }
+    
+    private BNode<T> getMaxHelper(BNode<T> node)
+    {
+	while (!node.isEmpty())
+	{
+	    node = node.getRight();
+	}
+	
+	return node; 
+    }
+    
+    public T getPredecessor(int key)
+    {
+	return null;
+    }
+    
+    public BNode<T> getPredecessorHelper(int key)
+    {
+	BNode<T> node = searchHelper(root, key);
+	
+	if (node.hasLeftChild())
+	{
+	    return getMaxHelper(node.getLeft());
+	}
+	else 
+	{
+	    node = node.getParent();
+	    
+	    while (node.getKey() > key)
+	    {
+		node = node.getParent();
+	    }
+	    
+	    return node;
+	}
+    }
+    
+    public T getSuccessor(int key)
+    {
+	return getSuccessorHelper(key).getValue();
+    }
+    
+    public BNode<T> getSuccessorHelper(int key)
+    {
+	BNode<T> node = searchHelper(root, key);
+	
+	if (node.hasRightChild())
+	{
+	    return getMinHelper(node.getRight());
+	}
+	else 
+	{
+	    node = node.getParent();
+	    
+	    while (node.getKey() < key)
+	    {
+		node = node.getParent();
+	    }
+	    
+	    return node;
+	}
+    }
+    
+    public boolean insert(T value, int key)
+    {
+	return insertHelper(root, value, key);
+    }
+    
+    private boolean insertHelper(BNode<T> node, T value, int key)
+    {
+	if (node.isEmpty())
+	{
+	    node.setValue(value);
+	    node.setKey(key);
+	    
+	    return true;
+	}
+	else
+	{
+	    if (key > node.getKey())
+		return insertHelper(node.getRight(), value, key);
+	    else if (key < node.getKey())
+		return insertHelper(node.getLeft(), value, key);
+	    
+	    return false;
+	}
+    }
+    
+    public T remove(int key)
+    {
+	return null;
+    }
+
+    public BNode<T> getRoot() 
+    {
+        return root;
+    }
+
+    public void setRoot(BNode<T> root) 
+    {
+        this.root = root;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,40 @@
+
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+	RBTree<Integer> rbtree = new RBTree<Integer>();
+	
+	rbtree.insert(42, 42);
+	rbtree.breadthTraversalPrint();
+
+	rbtree.insert(11, 11);
+	rbtree.breadthTraversalPrint();
+
+	rbtree.insert(1, 1);
+	rbtree.breadthTraversalPrint();
+
+	rbtree.insert(90, 90);
+	rbtree.breadthTraversalPrint();
+
+	rbtree.insert(39, 39);
+	rbtree.breadthTraversalPrint();
+
+	rbtree.insert(32, 32);
+	rbtree.breadthTraversalPrint();
+
+	rbtree.insert(9, 9);
+	rbtree.breadthTraversalPrint();
+
+	rbtree.insert(23, 23);
+	rbtree.breadthTraversalPrint();
+
+	rbtree.insert(19, 19); //destroi a parada feio
+
+	rbtree.breadthTraversalPrint();
+    }
+
+}
