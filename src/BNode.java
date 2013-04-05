16f418f565a0c722a6e9967e252243b1d87350b8
Andreza Medeiros <andreza.cmedeiros@gmail.com>
2013-04-05 14:43:31 -0300

    Consertando método para não dar nullPointerException

--- src/BNode.java
+++ src/BNode.java
@@ -1,131 +1,131 @@
 @SuppressWarnings("unchecked")
 public class BNode<T>
 {
-    private T value;
-    private int key;
-    private BNode<T> parent;
-    
-    protected BNode<?>[] children;
-    
-    public BNode(int numOfChildren)
-    {
-	this.key = 0;
-	children = new BNode<?>[numOfChildren];
-    }
+	private T value;
+	private int key;
+	private BNode<T> parent;
 
-    public BNode() 
-    {
-	this(2);
-    }
-    
-    public BNode<T> getLeft()
-    {
-	return (BNode<T>) children[0];
-    }
+	protected BNode<?>[] children;
 
-    public BNode<T> getRight()
-    {
-	return (BNode<T>) children[1];
-    }
-    
-    public T getValue() 
-    {
-	return value;
-    }
-    
-    public int getKey() 
-    {
-	return key;
-    }
-
-    public void setValue(T value) 
-    {
-	this.value = value;
-	initChildren();
-    }
-    
-    public void initChildren()
-    {
-	//quando um valor é setado, esse nó deixa de ser nulo,
-	//e seus filhos são inicializados
-	for (int i = 0; i < 2; i++)
+	public BNode(int numOfChildren)
 	{
-	    BNode<T> node = new BNode<T>();
-	    node.setParent(this);
-	    children[i] = node;
+		this.key = 0;
+		children = new BNode<?>[numOfChildren];
 	}
-    }
-    
-    public void setKey(int key) 
-    {
-        this.key = key;
-    }
-    
-    public void setLeft(BNode<T> node)
-    {
-	node.setParent(this);
-	children[0] = node;
-    }
-    
-    public void setRight(BNode<T> node)
-    {
-	node.setParent(this);
-	children[1] = node;
-    }
 
-    public boolean isEmpty() 
-    {
-	return this.key == 0;
-    }
+	public BNode() 
+	{
+		this(2);
+	}
 
-    public boolean hasChild(int num)
-    {
-	return children[num] != null && !((BNode<T>) children[num]).isEmpty();
-    }
+	public BNode<T> getLeft()
+	{
+		return (BNode<T>) children[0];
+	}
 
-    public boolean hasLeftChild()
-    {
-	return hasChild(0);
-    }
+	public BNode<T> getRight()
+	{
+		return (BNode<T>) children[1];
+	}
 
-    public boolean hasRightChild()
-    {
-	return hasChild(1);
-    }
-    
-    public boolean isLeft()
-    {
-	return (this == parent.getLeft());
-    }
-    
-    public boolean isRight()
-    {
-	return !isLeft();
-    }
-    
-    public BNode<T> getBrother()
-    {
-	boolean isLeft = isLeft();
-	return (isLeft ? parent.getRight() : parent.getLeft());
-    }
+	public T getValue() 
+	{
+		return value;
+	}
 
-    public BNode<T> getUncle()
-    {
-	return parent.getBrother();
-    }
-    
-    public BNode<T> getGrandfather()
-    {
-	return parent.getParent();
-    }
-    
-    public BNode<T> getParent() 
-    {
-	return parent;
-    }
+	public int getKey() 
+	{
+		return key;
+	}
 
-    public void setParent(BNode<T> parent) 
-    {
-	this.parent = parent;
-    }
+	public void setValue(T value) 
+	{
+		this.value = value;
+		initChildren();
+	}
+
+	public void initChildren()
+	{
+		//quando um valor ï¿½ setado, esse nï¿½ deixa de ser nulo,
+		//e seus filhos sï¿½o inicializados
+		for (int i = 0; i < 2; i++)
+		{
+			BNode<T> node = new BNode<T>();
+			node.setParent(this);
+			children[i] = node;
+		}
+	}
+
+	public void setKey(int key) 
+	{
+		this.key = key;
+	}
+
+	public void setLeft(BNode<T> node)
+	{
+		node.setParent(this);
+		children[0] = node;
+	}
+
+	public void setRight(BNode<T> node)
+	{
+		node.setParent(this);
+		children[1] = node;
+	}
+
+	public boolean isEmpty() 
+	{
+		return this.key == 0;
+	}
+
+	public boolean hasChild(int num)
+	{
+		return children[num] != null && !((BNode<T>) children[num]).isEmpty();
+	}
+
+	public boolean hasLeftChild()
+	{
+		return hasChild(0);
+	}
+
+	public boolean hasRightChild()
+	{
+		return hasChild(1);
+	}
+
+	public boolean isLeft()
+	{
+		return (this == parent.getLeft());
+	}
+
+	public boolean isRight()
+	{
+		return !isLeft();
+	}
+
+	public BNode<T> getBrother()
+	{
+		boolean isLeft = isLeft();
+		return (isLeft ? parent.getRight() : parent.getLeft());
+	}
+
+	public BNode<T> getUncle()
+	{
+		return parent.getBrother();
+	}
+
+	public BNode<T> getGrandfather()
+	{
+		return parent.getParent();
+	}
+
+	public BNode<T> getParent() 
+	{
+		return parent;
+	}
+
+	public void setParent(BNode<T> parent) 
+	{
+		this.parent = parent;
+	}
 }
--- src/BSTree.java
+++ src/BSTree.java
@@ -3,257 +3,258 @@ import java.util.Queue;
 
 public class BSTree<T> 
 {
-    private BNode<T> root;
-   
-    public BSTree()
-    {
-	root = new BNode<T>();
-    }
-    
-    public boolean isValid()
-    {
-	Queue<BNode<T>> queue = new ArrayDeque<BNode<T>>();
+	private BNode<T> root;
 
-	queue.offer(root);
-	
-	while (!queue.isEmpty())
+	public BSTree()
 	{
-	    BNode<T> node = queue.poll();
-	    
-	    if (node.isEmpty())
-	    {
-		continue;
-	    }
-	    
-	    if (node != getRoot())
-	    {
-		//se o no eh esquerdo e a chave dele eh maior do que a do pai
-		if (node.isLeft() && node.getKey() > node.getParent().getKey())
-		    return false;
-		
-		//se o no eh direito e a chave dele eh menor do que a do pai
-		if (node.isRight() && node.getKey() < node.getParent().getKey())
-		    return false;
-		
-		queue.offer(node.getLeft());
-		queue.offer(node.getRight());
-	    }
+		root = new BNode<T>();
 	}
-	
-	return true;
-    }
-    
-    
-    
-    public void breadthTraversalPrint()
-    {
-	Queue<BNode<T>> queue = new ArrayDeque<BNode<T>>();
 
-	queue.offer(root);
-	
-	while (!queue.isEmpty())
+	public boolean isValid()
 	{
-	    BNode<T> node = queue.poll();
-	    
-	    if (node.isEmpty())
-	    {
-		System.out.println("E");
-		continue;
-	    }
-	    
-	    System.out.println(node.getValue());
-	    queue.offer(node.getLeft());
-	    queue.offer(node.getRight());
+		Queue<BNode<T>> queue = new ArrayDeque<BNode<T>>();
+
+		queue.offer(root);
+
+		while (!queue.isEmpty())
+		{
+			BNode<T> node = queue.poll();
+
+			if (node.isEmpty())
+			{
+				continue;
+			}
+
+			if (node != getRoot())
+			{
+				//se o no eh esquerdo e a chave dele eh maior do que a do pai
+				if (node.isLeft() && node.getKey() > node.getParent().getKey())
+					return false;
+
+				//se o no eh direito e a chave dele eh menor do que a do pai
+				if (node.isRight() && node.getKey() < node.getParent().getKey())
+					return false;
+
+				queue.offer(node.getLeft());
+				queue.offer(node.getRight());
+			}
+		}
+
+		return true;
 	}
-    }
-    
-    public void printInOrder()
-    {
-	inorder(root);
-    }
-    
-    private void inorder(BNode<T> node)
-    {
-	if (node.isEmpty())
-	    return;
-	
-	inorder(node.getLeft());
-	System.out.println(node.getValue());
-	inorder(node.getRight());
-    }
-    
-    public void printPostOrder()
-    {
-	postorder(root);
-    }
-    
-    private void postorder(BNode<T> node)
-    {
-	if (node.isEmpty())
-	    return;
-	
-	postorder(node.getLeft());
-	postorder(node.getRight());
-	System.out.println(node.getValue());
-    }
-    
-    public void printPreOrder()
-    {
-	preorder(root);
-    }
-    
-    private void preorder(BNode<T> node)
-    {
-	if (node.isEmpty())
-	    return;
-	
-	System.out.println(node.getValue());	
-	preorder(node.getLeft());
-	preorder(node.getRight());
-    }
-    
-    public T search(int key)
-    {
-	return searchHelper(root, key).getValue();
-    }
-    
-    private BNode<T> searchHelper(BNode<T> node, int key)
-    {
-	if (node.isEmpty())
-	    return null;
-	
-	if (key > node.getKey())
-	    return searchHelper(node.getRight(), key);
-	else if (key < node.getKey())
-	    return searchHelper(node.getLeft(), key);
-	
-	return node;
-    }
-    
-    public T getMin()
-    {
-	return getMinHelper(root).getValue();
-    }
-    
-    private BNode<T> getMinHelper(BNode<T> node)
-    {	
-	while (!node.isEmpty())
+
+	public void breadthTraversalPrint()
 	{
-	    node = node.getLeft();
+		Queue<BNode<T>> queue = new ArrayDeque<BNode<T>>();
+
+		queue.offer(root);
+
+		while (!queue.isEmpty())
+		{
+			BNode<T> node = queue.poll();
+
+			if (node.isEmpty())
+			{
+				System.out.println("E");
+				continue;
+			}
+
+			System.out.println(node.getValue());
+			queue.offer(node.getLeft());
+			queue.offer(node.getRight());
+		}
 	}
-	
-	return node;
-    }
-    
-    public T getMax()
-    {
-	return getMaxHelper(root).getValue();
-    }
-    
-    private BNode<T> getMaxHelper(BNode<T> node)
-    {
-	while (!node.isEmpty())
+
+	public void printInOrder()
 	{
-	    node = node.getRight();
+		inorder(root);
 	}
-	
-	return node; 
-    }
-    
-    public T getPredecessor(int key)
-    {
-	BNode<T> predecessorNode = getPredecessorHelper(key);
-	return predecessorNode == null ? null : predecessorNode.getValue();
-    }
-    
-    public BNode<T> getPredecessorHelper(int key)
-    {
-	BNode<T> node = searchHelper(root, key);
-	
-	if (node.hasLeftChild())
+
+	private void inorder(BNode<T> node)
 	{
-	    return getMaxHelper(node.getLeft());
+		if (node.isEmpty())
+			return;
+
+		inorder(node.getLeft());
+		System.out.println(node.getValue());
+		inorder(node.getRight());
 	}
-	else 
+
+	public void printPostOrder()
 	{
-	    if (node == getRoot())
+		postorder(root);
+	}
+
+	private void postorder(BNode<T> node)
+	{
+		if (node.isEmpty())
+			return;
+
+		postorder(node.getLeft());
+		postorder(node.getRight());
+		System.out.println(node.getValue());
+	}
+
+	public void printPreOrder()
+	{
+		preorder(root);
+	}
+
+	private void preorder(BNode<T> node)
+	{
+		if (node.isEmpty())
+			return;
+
+		System.out.println(node.getValue());	
+		preorder(node.getLeft());
+		preorder(node.getRight());
+	}
+
+	public T search(int key)
+	{
+		BNode<T> findNode = searchHelper(root, key);
+		return findNode == null ? null : findNode.getValue();
+	}
+
+	private BNode<T> searchHelper(BNode<T> node, int key)
+	{
+		if (node.isEmpty())
+			return null;
+
+		if (key > node.getKey())
+			return searchHelper(node.getRight(), key);
+		else if (key < node.getKey())
+			return searchHelper(node.getLeft(), key);
+
+		return node;
+	}
+
+	public T getMin()
+	{
+		return getMinHelper(root).getValue();
+	}
+
+	private BNode<T> getMinHelper(BNode<T> node)
+	{	
+		while (!node.isEmpty())
+		{
+			node = node.getLeft();
+		}
+
+		return node;
+	}
+
+	public T getMax()
+	{
+		return getMaxHelper(root).getValue();
+	}
+
+	private BNode<T> getMaxHelper(BNode<T> node)
+	{
+		while (!node.isEmpty())
+		{
+			node = node.getRight();
+		}
+
+		return node; 
+	}
+
+	public T getPredecessor(int key)
+	{
+		BNode<T> predecessorNode = getPredecessorHelper(key);
+		return predecessorNode == null ? null : predecessorNode.getValue();
+	}
+
+	public BNode<T> getPredecessorHelper(int key)
+	{
+		BNode<T> node = searchHelper(root, key);
+
+		if (node.hasLeftChild())
+		{
+			return getMaxHelper(node.getLeft());
+		}
+		else 
+		{
+			if (node == getRoot())
+				return null;
+
+			node = node.getParent();
+
+			while (node.getKey() > key)
+			{
+				node = node.getParent();
+			}
+
+			return node;
+		}
+	}
+
+	public T getSuccessor(int key)
+	{
+		BNode<T> successorNode = getSuccessorHelper(key);
+		return successorNode == null ? null : successorNode.getValue();    
+	}
+
+	public BNode<T> getSuccessorHelper(int key)
+	{
+		BNode<T> node = searchHelper(root, key);
+
+		if (node.hasRightChild())
+		{
+			return getMinHelper(node.getRight());
+		}
+		else 
+		{
+			node = node.getParent();
+
+			while (node.getKey() < key)
+			{
+				node = node.getParent();
+			}
+
+			return node;
+		}
+	}
+
+	public boolean insert(T value, int key)
+	{
+		return insertHelper(root, value, key);
+	}
+
+	private boolean insertHelper(BNode<T> node, T value, int key)
+	{
+		if (node.isEmpty())
+		{
+			node.setValue(value);
+			node.setKey(key);
+
+			return true;
+		}
+		else
+		{
+			if (key > node.getKey())
+				return insertHelper(node.getRight(), value, key);
+			else if (key < node.getKey())
+				return insertHelper(node.getLeft(), value, key);
+
+			return false;
+		}
+	}
+
+	public T remove(int key)
+	{
+		//TODO
+		searchHelper(root, key);
 		return null;
-	    
-	    node = node.getParent();
-	    
-	    while (node.getKey() > key)
-	    {
-		node = node.getParent();
-	    }
-	    
-	    return node;
 	}
-    }
-    
-    public T getSuccessor(int key)
-    {
-	BNode<T> successorNode = getSuccessorHelper(key);
-	return successorNode == null ? null : successorNode.getValue();    
-    }
-    
-    public BNode<T> getSuccessorHelper(int key)
-    {
-	BNode<T> node = searchHelper(root, key);
-	
-	if (node.hasRightChild())
-	{
-	    return getMinHelper(node.getRight());
-	}
-	else 
-	{
-	    node = node.getParent();
-	    
-	    while (node.getKey() < key)
-	    {
-		node = node.getParent();
-	    }
-	    
-	    return node;
-	}
-    }
-    
-    public boolean insert(T value, int key)
-    {
-	return insertHelper(root, value, key);
-    }
-    
-    private boolean insertHelper(BNode<T> node, T value, int key)
-    {
-	if (node.isEmpty())
-	{
-	    node.setValue(value);
-	    node.setKey(key);
-	    
-	    return true;
-	}
-	else
-	{
-	    if (key > node.getKey())
-		return insertHelper(node.getRight(), value, key);
-	    else if (key < node.getKey())
-		return insertHelper(node.getLeft(), value, key);
-	    
-	    return false;
-	}
-    }
-    
-    public T remove(int key)
-    {
-	throw new UnsupportedOperationException("Só inserção, nada de remover!");
-    }
 
-    public BNode<T> getRoot() 
-    {
-        return root;
-    }
+	public BNode<T> getRoot() 
+	{
+		return root;
+	}
 
-    public void setRoot(BNode<T> root) 
-    {
-        this.root = root;
-    }
+	public void setRoot(BNode<T> root) 
+	{
+		this.root = root;
+	}
 }
